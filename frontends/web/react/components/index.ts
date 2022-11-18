584a14424b5484d44dcad7cd097e6d92014e34f8
João Magalhães <joamag@gmail.com>
2022-11-18 16:50:10 +0000

    feat: new panel tab support

--- frontends/web/react/components/index.ts
+++ frontends/web/react/components/index.ts
@@ -14,6 +14,7 @@ export * from "./modal/modal";
 export * from "./overlay/overlay";
 export * from "./pair/pair";
 export * from "./panel-split/panel-split";
+export * from "./panel-tab/panel-tab";
 export * from "./paragraph/paragraph";
 export * from "./registers-gb/registers-gb";
 export * from "./section/section";
---
+++ frontends/web/react/components/panel-tab/panel-tab.tsx
@@ -0,0 +1,21 @@
+import React, { FC, ReactNode } from "react";
+
+import "./panel-split.css";
+
+type PanelTabProps = {
+    tabs?: ReactNode[];
+    style?: string[];
+};
+
+export const PanelTab: FC<PanelTabProps> = ({
+    tabs,
+    style = []
+}) => {
+    const classes = () => ["panel-tab", ...style].join(" ");
+    return (
+        <div className={classes()}>
+        </div>
+    );
+};
+
+export default PanelTab;
