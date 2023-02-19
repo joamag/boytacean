710445dd4bbb89204be183ec9e49e524e94528e6
João Magalhães <joamag@gmail.com>
2023-02-19 18:39:23 +0000

    feat: more env variable obtained

--- src/gen/mock.rs
+++ src/gen/mock.rs
@@ -2,9 +2,14 @@
 
 pub const COMPILATION_DATE: &str = "-";
 pub const COMPILATION_TIME: &str = "-";
+pub const NAME: &str = "-";
 pub const VERSION: &str = "x.x.x";
 pub const COMPILER: &str = "rustc";
 pub const COMPILER_VERSION: &str = "x.x.x";
+pub const HOST: &str = "-";
+pub const TARGET: &str = "-";
+pub const PROFILE: &str = "-";
+pub const MAKEFLAGS: &str = "-";
 pub const FEATURES: [&str; 1] = ["cpu"];
 pub const PLATFORM_CPU_BITS: &str = "64";
 pub const DEFAULT_THREAD_POOL_SIZE: usize = 1;
