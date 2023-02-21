5d5e2743abc0acba5e351872521ffe4decbbc292
João Magalhães <joamag@gmail.com>
2023-02-21 16:25:39 +0000

    fix: docs.rs support by creating gen-mock feture Which allows usage of the static version of the generated values.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,7 +17,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Fixed
 
-*
+* Build process for the docs.rs website
 
 ## [0.6.11] - 2023-02-21
 
--- src/gen/mod.rs
+++ src/gen/mod.rs
@@ -1,12 +1,12 @@
 //! Constants that define the current build and execution environment.
 
-#[cfg(feature = "readonly")]
+#[cfg(feature = "gen-mock")]
 pub mod mock;
-#[cfg(feature = "readonly")]
+#[cfg(feature = "gen-mock")]
 pub use self::mock::*;
 
 #[rustfmt::skip]
-#[cfg(not(feature = "readonly"))]
+#[cfg(not(feature = "gen-mock"))]
 pub mod build;
-#[cfg(not(feature = "readonly"))]
+#[cfg(not(feature = "gen-mock"))]
 pub use self::build::*;
