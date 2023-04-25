4fb48b8ba28a7ec2c9d9e8178f3f0d496122a221
João Magalhães <joamag@gmail.com>
2023-04-25 09:28:32 +0100

    chore: better boot dump

--- res/boot/boot_dump.py
+++ res/boot/boot_dump.py
@@ -3,19 +3,35 @@
 
 import sys
 
-def print_buffer(filename):
+
+def print_buffer(filename, mode="buffer"):
     file = open(filename, "rb")
-    try: data = file.read()
-    finally: file.close()
+    try:
+        data = file.read()
+    finally:
+        file.close()
 
-    buffer = [str(ord(byte)) for byte in data]
-    buffer_s = ", ".join(buffer)
+    if mode == "buffer":
+        buffer = [str(ord(byte)) for byte in data]
+        buffer_s = ", ".join(buffer)
 
-    print("[%s]" % buffer_s)
+        print("[%s]" % buffer_s)
+
+    elif mode == "hex":
+        counter = 0
+        for byte in data:
+            print("[0x%04x] 0x%02x" % (counter, ord(byte)))
+            counter += 1
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Missing arguments")
         exit(1)
 
-    print_buffer(sys.argv[1])
+    mode = "buffer"
+
+    if len(sys.argv) > 2:
+        mode = sys.argv[2]
+
+    print_buffer(sys.argv[1], mode=mode)
