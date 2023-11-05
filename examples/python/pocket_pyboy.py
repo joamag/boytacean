94dd3713d725a0e97e491aa43f86429764380278
João Magalhães <joamag@gmail.com>
2023-11-05 22:47:22 +0000

    chore: new pyboy

--- examples/python/pocket_pyboy.py
+++ examples/python/pocket_pyboy.py
@@ -1,10 +1,11 @@
 from time import time
 from pyboy import PyBoy
 
-with PyBoy("../../res/roms/demo/pocket.gb", disable_renderer=True) as pyboy:
+with PyBoy("../../res/roms/demo/pocket.gb", disable_renderer=False) as pyboy:
     pyboy.set_emulation_speed(0)
     print(pyboy.cartridge_title())
     start = time()
     for _ in range(6000):
         pyboy.tick()
     print(f"Time taken: {(time() - start)}")
+    pyboy.screen_image().save("pocket_pyboy.png")
