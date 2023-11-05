b9bc13746ca152f295dbde47fa794145e0079320
João Magalhães <joamag@gmail.com>
2023-11-05 22:16:43 +0000

    chore: new pyboy demo

--- examples/python/pocket.py
+++ examples/python/pocket.py
@@ -1,8 +1,11 @@
-import boytacean
+from time import time
+from boytacean import GameBoy
 
-gb = boytacean.GameBoy()
+gb = GameBoy()
 gb.load()
 gb.load_rom("../../res/roms/demo/pocket.gb")
+start = time()
 for _ in range(6000):
     gb.next_frame()
+print(f"Time taken: {(time() - start)}")
 gb.save_image("pocket.png")
