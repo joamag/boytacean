60e680a179ba14710cdd9a74e380a03af292dd15
João Magalhães <joamag@gmail.com>
2024-02-18 23:21:50 +0000

    chore: improved test code

--- src/python/boytacean/test/base.py
+++ src/python/boytacean/test/base.py
@@ -4,18 +4,17 @@ from os.path import dirname, realpath, join
 
 from boytacean import GameBoy
 
+CURRENT_DIR = dirname(realpath(__file__))
+POCKET_ROM_PATH = join(CURRENT_DIR, "../../../../res/roms/demo/pocket.gb")
 
 class BaseTest(unittest.TestCase):
 
     def test_pocket(self):
-        CURRENT_DIR = dirname(realpath(__file__))
-        ROM_PATH = join(CURRENT_DIR, "../../../../res/roms/demo/pocket.gb")
-        FRAME_COUNT = 600
-        LOAD_GRAPHICS = False
-
         gb = GameBoy(
-            apu_enabled=False, serial_enabled=False, load_graphics=LOAD_GRAPHICS
+            apu_enabled=False, serial_enabled=False, load_graphics=False
         )
-        gb.load_rom(ROM_PATH)
-        for _ in range(FRAME_COUNT):
+        gb.load_rom(POCKET_ROM_PATH)
+        for _ in range(600):
             gb.next_frame()
+
+        self.assertEqual(gb.rom_title, "POCKET-DEMO")
