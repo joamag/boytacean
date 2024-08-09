9c6c283fdbbcb4727738b3b1fe47d58bd8e46b27
João Magalhães <joamag@gmail.com>
2024-08-09 21:16:18 +0100

    fix: new ignore values

--- crates/common/src/lib.rs
+++ crates/common/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::uninlined_format_args)]
+
 pub mod bench;
 pub mod error;
 pub mod util;
--- crates/hashing/src/lib.rs
+++ crates/hashing/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::uninlined_format_args)]
+
 pub mod crc32;
 pub mod crc32c;
 pub mod hash;
