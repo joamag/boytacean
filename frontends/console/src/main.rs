bcda8d4a418a084a798b884c607fb64b005239d2
João Magalhães <joamag@gmail.com>
2025-04-27 11:46:56 +0100

    chore: better cycles handling

--- frontends/console/src/main.rs
+++ frontends/console/src/main.rs
@@ -5,15 +5,20 @@ const REBOOTS: u32 = 500;
 
 fn main() {
     let mut game_boy = GameBoy::new(Some(GameBoyMode::Dmg));
-    let mut cycles = 0_u32;
+    let mut cycles = 0_u64;
     let start = Instant::now();
     println!("Running {} reboots...", REBOOTS);
     for _ in 0..REBOOTS {
         game_boy.reset();
         game_boy.load(true).unwrap();
         game_boy.load_rom_empty().unwrap();
-        cycles += game_boy.step_to(0x0100);
+        cycles += game_boy.step_to(0x0100) as u64;
     }
     let elapsed = start.elapsed();
-    println!("Ran {} cycles in {:?}", cycles, elapsed);
+    let elapsed_s = elapsed.as_secs_f64();
+    let frequency_mhz = cycles as f64 / elapsed_s / 1000.0 / 1000.0;
+    println!(
+        "Ran {} cycles in {:?} ({:.2} Mhz)",
+        cycles, elapsed, frequency_mhz
+    );
 }
