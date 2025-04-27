0e9eaaee0dc232242c1a711cfafbcb8ee37b0188
João Magalhães <joamag@gmail.com>
2025-04-27 11:01:20 +0100

    chore: extract reboot count into a constant

--- frontends/console/src/main.rs
+++ frontends/console/src/main.rs
@@ -1,11 +1,14 @@
 use boytacean::gb::{GameBoy, GameBoyMode};
 use std::time::Instant;
 
+const REBOOTS: u32 = 500;
+
 fn main() {
     let mut game_boy = GameBoy::new(Some(GameBoyMode::Dmg));
     let mut cycles = 0_u32;
     let start = Instant::now();
-    for _ in 0..500 {
+    println!("Running {} reboots...", REBOOTS);
+    for _ in 0..REBOOTS {
         game_boy.reset();
         game_boy.load(true).unwrap();
         game_boy.load_rom_empty().unwrap();
