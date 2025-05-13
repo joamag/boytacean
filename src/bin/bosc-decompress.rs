ab661d6dbd140dc3de69ccb01ca86bc30e73dc79
João Magalhães <joamag@gmail.com>
2025-05-13 22:51:26 +0100

    chore: simplify argument handling in bosc-decompress

--- src/bin/bosc-decompress.rs
+++ src/bin/bosc-decompress.rs
@@ -1,13 +1,13 @@
 use boytacean::state::{SaveStateFormat, Serialize, StateManager};
-use std::{env, fs::File, io::Read, path::Path};
+use std::{env::args, error::Error, fs::File, io::Read, path::Path};
 
 fn print_usage() {
     println!("Usage: bosc-decompress <input_file> [output_file]");
     println!("If output_file is not specified, it will use input_file with .bos extension");
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = env::args().collect();
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = args().collect();
 
     if args.len() < 2 {
         print_usage();
