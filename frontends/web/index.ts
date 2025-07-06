8720496d66e5ab54a6a9aefa21d37ddc90add31c
João Magalhães <joamag@gmail.com>
2025-07-06 16:08:23 +0100

    fix(web): enable shader usage in WebGL setup

--- frontends/web/index.ts
+++ frontends/web/index.ts
@@ -59,11 +59,11 @@ const BACKGROUNDS = [
         backgrounds: BACKGROUNDS
     });
 
-    //if (shader) {
-    setTimeout(async () => {
-        await setupWebGL(emulator, "crt");
-    }, 3000);
-    //}
+    if (shader) {
+        setTimeout(async () => {
+            await setupWebGL(emulator, shader);
+        }, 3000);
+    }
 
     // sets the emulator in the global scope this is useful
     // to be able to access the emulator from global functions
