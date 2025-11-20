ad63759ea538e303bd7e4f9e1cc276ac08297f15
João Magalhães <joamag@gmail.com>
2025-11-20 09:08:28 +0000

    chore: enhance documentation for generate_data and multiply_array_size functions

--- crates/common/src/bench.rs
+++ crates/common/src/bench.rs
@@ -3,6 +3,16 @@
 //! Most of the these functions are used to generate data for benchmarking
 //! and used in Criterion benchmarks.
 
+/// Generate data with repeating patterns for benchmarking.
+///
+/// The generated data consists of repeating sequences of specific byte patterns
+/// to simulate realistic data for compression benchmarks.
+///
+/// # Arguments
+/// * `size` - The total size of the generated data in bytes.
+///
+/// # Returns
+/// A `Vec<u8>` containing the generated data.
 pub fn generate_data(size: usize) -> Vec<u8> {
     let patterns: [&[u8]; 6] = [
         b"aaaaa",
@@ -32,6 +42,16 @@ pub fn generate_data(size: usize) -> Vec<u8> {
     data
 }
 
+/// Multiply the size of an array by a given multiplier, returning
+/// a new array with repeated elements.
+///
+/// # Arguments
+/// * `arr` - The input array to be multiplied.
+/// * `multiplier` - The factor by which to multiply the size of the array.
+///
+/// # Returns
+/// A new `Vec<T>` containing the elements of the original array repeated
+/// according to the specified multiplier.
 pub fn multiply_array_size<T: Clone>(arr: &[T], multiplier: usize) -> Vec<T> {
     let mut new_arr = Vec::with_capacity(arr.len() * multiplier);
     for _ in 0..multiplier {
